80d0c6579026f367ee852abef9d9dde4e084d42f
vis100304 <vismayabhandari03@gmail.com>
2025-08-04 19:02:01 +0530

    Add EV forecast Streamlit app

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -255,8 +255,4 @@ if multi_counties:
 
 st.success("Forecast complete")
 
-
 st.markdown("Prepared for the **AICTE Internship Cycle 2 by S4F**")
-
-st.markdown("Prepared for the **AICTE Internship Cycle 2 by S4F**")
- 
